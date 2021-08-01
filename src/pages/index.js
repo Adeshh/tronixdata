25993df2598faca44f41a8de801ec5e494f50995
Adesh Patil <44741222+Adeshh@users.noreply.github.com>
2021-08-01 16:30:00 +0530

    class name changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,18 +21,18 @@ export default function Home() {
         <Image className=" " src={BtmDesign} />
       </div>
       <OurServices/>
-      <section class="tracking-normal text-white gradient flex flex-col justify-center items-center w-screen  ">
+      <section className="tracking-normal text-white gradient flex flex-col justify-center items-center w-screen  ">
         <Image className=" " src={BtmDesign2} />
-        <h1 class="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
+        <h1 className="w-full my-2 text-5xl font-bold leading-tight text-center text-white">
           Footer
         </h1>
-        <div class="w-full mb-4">
-          <div class="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
+        <div className="w-full mb-4">
+          <div className="h-1 mx-auto bg-white w-1/6 opacity-25 my-0 py-0 rounded-t"></div>
         </div>
-        <h3 class="my-4 text-3xl leading-tight">
+        <h3 className="my-4 text-3xl leading-tight">
           Importent Links
         </h3>
-        <button class="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
+        <button className="mx-auto lg:mx-0 hover:underline bg-white text-gray-800 font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out">
           Action!
         </button>
       </section>
